bfbe49cdf607591378fd1bde20274faf8471195e
Jia Chen Wee <32972710+jaycvevo@users.noreply.github.com>
2021-03-13 04:05:16 -0500

    created rating screen

--- client/App.js
+++ client/App.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
 
 import ExerciseStack from './app/routes/exerciseStack'
 import ProgressStack from './app/routes/progressStack'
@@ -12,7 +14,28 @@ const Tab = createBottomTabNavigator();
 export default function TabNavigator() {
     return (
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Exercise') {
+                iconName = focused ? 'barbell' : 'barbell-outline';
+              } else if (route.name === 'Progress') {
+                iconName = focused ? 'hourglass' : 'hourglass-outline';
+              } else if (route.name === 'Data') {
+                iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+              }
+  
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'royalblue',
+            inactiveTintColor: 'gray',
+          }}
+        >
           <Tab.Screen name="Exercise" component={ExerciseStack} />
           <Tab.Screen name="Progress" component={ProgressStack} />
           <Tab.Screen name="Data" component={DataStack} />
